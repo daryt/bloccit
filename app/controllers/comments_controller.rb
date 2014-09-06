7e938cf61144c976306872f99de8f45f841d8985
Tyler Dary <daryt@msn.com>
2014-09-06 15:27:29 +0000

    added destroying with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  
+  respond_to :html, :js
+
   def destroy
     #@topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
@@ -8,10 +9,11 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@post.topic, @post]
-    else
+     else
       flash[:error] = "Comment couldn't be deleted.  Try again."
-      redirect_to [@post.topic, @post]
+    end
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 
